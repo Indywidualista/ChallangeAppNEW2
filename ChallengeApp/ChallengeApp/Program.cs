6de6e7c68b71ef39dc8f9a74ff7f1db358a5a067
Indywidualista <godlinski.jakub@gmail.com>
2023-09-16 15:18:07 +0200

    Zadanie domowe dzien 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,21 +1,18 @@
 ﻿using Microsoft.VisualBasic.FileIO;
 
-var age = 19;
+var age = 33;
 var name = "Ewa";
-var gender = "male";
+var gender = "female";
 
-if (age == 33 && gender == "female")
+if (age == 33 && gender == "female" && name == "Ewa")
 {
-    if (name == "Ewa")
-    {
-        Console.WriteLine("Ewa, lat 33");
-    }
+    Console.WriteLine("Ewa, lat 33");
 }
 else if (age < 30 && gender == "female")
 {
     Console.WriteLine("Kobieta poniżej 30 lat");
 }
-else if (age < 18 && !(gender == "female"))
+if (age < 18 && !(gender == "female"))
 {
     Console.WriteLine("Niepełnoletni Mężczyzna");
 }
@@ -23,7 +20,7 @@ else if (age > 33)
 {
     Console.WriteLine("Osoba ma więcej niż 33 lata");
 }
-if (age > 17 && gender == "male" || gender == "female")
+if (age > 17)
 {
     Console.WriteLine("Osoba jest pełnoletnia");
 }
